38d522e860284f6ca4f1001ad61d6c926c8e30dc
mandp80 <mandp80@yahoo.com>
2015-03-31 19:55:31 +0530

    Create BaggageSystemConstants.java

---
+++ BaggageSystemConstants.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2015, Manishkumar Prajapati. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+package com.barclays.baggagesys.constant;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BaggageSystemConstants.
+ */
+public final class BaggageSystemConstants {
+
+	/** The Constant GRAPH_ALGO_DIJKSTRA. */
+	public static final String GRAPH_ALGO_DIJKSTRA = "GRAPH_ALGO_DIJKSTRA";
+
+	/** The Constant NODE_Concourse_A_Ticketing. */
+	public static final String NODE_CONCOURSE_A_TICKETING = "Concourse_A_Ticketing";
+
+	/** The Constant NODE_BaggageClaim. */
+	public static final String NODE_BAGGAGE_CLAIM = "BaggageClaim";
+
+	/** The Constant NODE_A1. */
+	public static final String NODE_A1 = "A1";
+
+	/** The Constant NODE_A2. */
+	public static final String NODE_A2 = "A2";
+
+	/** The Constant NODE_A3. */
+	public static final String NODE_A3 = "A3";
+
+	/** The Constant NODE_A4. */
+	public static final String NODE_A4 = "A4";
+
+	/** The Constant NODE_A5. */
+	public static final String NODE_A5 = "A5";
+
+	/** The Constant NODE_A6. */
+	public static final String NODE_A6 = "A6";
+
+	/** The Constant NODE_A7. */
+	public static final String NODE_A7 = "A7";
+
+	/** The Constant NODE_A8. */
+	public static final String NODE_A8 = "A8";
+
+	/** The Constant NODE_A9. */
+	public static final String NODE_A9 = "A9";
+
+	/** The Constant NODE_A10. */
+	public static final String NODE_A10 = "A10";
+
+	/** The Constant FLIGHTID_UA10. */
+	public static final String FLIGHTID_UA10 = "UA10";
+
+	/** The Constant FLIGHTID_UA11. */
+	public static final String FLIGHTID_UA11 = "UA11";
+
+	/** The Constant FLIGHTID_UA12. */
+	public static final String FLIGHTID_UA12 = "UA12";
+
+	/** The Constant FLIGHTID_UA13. */
+	public static final String FLIGHTID_UA13 = "UA13";
+
+	/** The Constant FLIGHTID_UA14. */
+	public static final String FLIGHTID_UA14 = "UA14";
+
+	/** The Constant FLIGHTID_UA15. */
+	public static final String FLIGHTID_UA15 = "UA15";
+
+	/** The Constant FLIGHTID_UA16. */
+	public static final String FLIGHTID_UA16 = "UA16";
+
+	/** The Constant FLIGHTID_UA17. */
+	public static final String FLIGHTID_UA17 = "UA17";
+
+	/** The Constant FLIGHTID_UA18. */
+	public static final String FLIGHTID_UA18 = "UA18";
+
+	/** The Constant FLIGHTID_ARRIVAL. */
+	public static final String FLIGHTID_ARRIVAL = "ARRIVAL";
+
+	/** The Constant FLIGHT_DEST_MIA. */
+	public static final String FLIGHT_DEST_MIA = "MIA";
+
+	/** The Constant FLIGHT_DEST_LAX. */
+	public static final String FLIGHT_DEST_LAX = "LAX";
+
+	/** The Constant FLIGHT_DEST_JFK. */
+	public static final String FLIGHT_DEST_JFK = "JFK";
+
+	/** The Constant FLIGHT_DEST_MHT. */
+	public static final String FLIGHT_DEST_MHT = "MHT";
+
+	/** The Constant BAG_NUMBER_0001. */
+	public static final String BAG_NUMBER_0001 = "0001";
+
+	/** The Constant BAG_NUMBER_0002. */
+	public static final String BAG_NUMBER_0002 = "0002";
+
+	/** The Constant BAG_NUMBER_0003. */
+	public static final String BAG_NUMBER_0003 = "0003";
+
+	/** The Constant BAG_NUMBER_0004. */
+	public static final String BAG_NUMBER_0004 = "0004";
+
+	/** The Constant BAG_NUMBER_0005. */
+	public static final String BAG_NUMBER_0005 = "0005";
+
+}
